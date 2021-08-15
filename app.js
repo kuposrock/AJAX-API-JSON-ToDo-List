572136d0ec8fea812e29488da6530d269c11b908
Matt <kuposrock@gmail.com>
2021-08-14 18:33:42 -0700

    Javascript working

--- app.js
+++ app.js
@@ -1,42 +1,81 @@
 
-var data=[];
+var data = [];
 var listContainer = document.getElementsByClassName("listContainer");
+var checkboxes = document.getElementsByClassName("itemCheckBox");
 
-document.addEventListener("DOMContentLoaded", getData());
-function getData(){
-const xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        var jsonData = xhr.responseText;
-        data = JSON.parse(jsonData);
-        console.log(data);
-    } 
-};
-xhr.open("GET", "https://api.vschool.io/matthewgrahek/todo", true);
-xhr.send();
+for(checks of checkboxes){
+    if(checks.checked){
+    console.log("checked");
+    }
 }
 
+document.addEventListener("DOMContentLoaded", getData());
+function getData() {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var jsonData = xhr.responseText;
+            data = JSON.parse(jsonData);
+            console.log(data);
+            parseDataToWebsite(data);
+        }
+    };
+    xhr.open("GET", "https://api.vschool.io/matthewgrahek/todo", true);
+    xhr.send();
+}
 
-function createPost(){
+function parseDataToWebsite(items) {
+    for (i in items) {
+        createIndividualPost(items[i], i);
+    }
+}
+
+function createIndividualPost(post, number) {
+
     var listItem = document.createElement("div");
     listItem.classList.add("listItem");
 
     var itemNumber = document.createElement("div");
-    itemNumber.classList.add("listNumber");
+    itemNumber.classList.add("itemNumber");
+    itemNumber.innerHTML = parseInt(number) + 1 + ". ";
 
     var itemTitle = document.createElement("div");
     itemTitle.classList.add("itemTitle");
+    itemTitle.innerText = (post.title === undefined) ? "Untitled": " "+ post.title;
 
     var img = document.createElement("img");
+    (post.imgUrl === undefined) ? img.style.display ='none' :img.src = post.imgUrl;
 
-    var itemDescript = document.createElement("div");
-    itemDescript.classList.add("itemDescript");
+    var itemDescription = document.createElement("div");
+    itemDescription.classList.add("itemDescription");
+    itemDescription.innerText = (post.description === undefined) ? "No" : post.description;
 
     var imgTextContainer = document.createElement("div");
     imgTextContainer.classList.add("imgTextContainer");
-}
 
+    var checkBox = document.createElement("input");
+    checkBox.setAttribute("type", "checkbox");
+    checkBox.classList.add("itemCheckBox");
+    
 
+    var editButton = document.createElement("button");
+    editButton.classList.add("editButton");
+    editButton.innerText = "Edit";
+
+    var deleteButton = document.createElement("button");
+    deleteButton.classList.add("deleteButton");
+    deleteButton.innerText = "Delete";
+
+    listItem.appendChild(itemNumber);
+    listItem.appendChild(itemTitle);
+    imgTextContainer.appendChild(img);
+    imgTextContainer.appendChild(itemDescription);
+    listItem.appendChild(imgTextContainer);
+    listItem.appendChild(checkBox);
+    listItem.appendChild(editButton);
+    listItem.appendChild(deleteButton);
+    listContainer[0].appendChild(listItem);
+    }
 
 // const xhr = new XMLHttpRequest();
 // xhr.onreadystatechange = function () {
