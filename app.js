bec0e66a7c6564ef39b56c3ab6f6908ccd6cadee
Matt <kuposrock@gmail.com>
2021-08-17 09:01:51 -0700

    Finished To Do list

--- app.js
+++ app.js
@@ -1,40 +1,133 @@
 
+
+function getData(){
+var postData = axios.get('https://api.vschool.io/matthewgrahek/todo').then(assignResponseToVariable).catch(error => {
+    console.log(error);
+  });
+}
+getData();
+
 var data = [];
 var listContainer = document.getElementsByClassName("listContainer");
 var checkboxes = document.getElementsByClassName("itemCheckBox");
+var editButtons = document.getElementsByClassName("editButton");
+var deleteButtons = document.getElementsByClassName("deleteButton");
+var form = document.listForm;
 
 
+form.addEventListener("submit",function(event){
+    event.preventDefault();
+    var title = form.inputTitle.value;
+    var description = form.inputDesc.value;
+    var url = form.inputURL.value;
+    var post = new createPost(title,description,url);
+    sendData(post);
+});
 
-for(var checkbox of checkboxes){
+function addFunctionality() {
 
-    checkbox.addEventListener('change', function(e) {
-        if (this.checked) {
-          
-        } else {
-          console.log("Checkbox is not checked..");
-        }
-      });
+    for (var deleteButton of deleteButtons) {
+        deleteButton.addEventListener('click', function (e) {
+            var postNumber = parseInt(e.target.parentNode.querySelector('.itemNumber').innerText);
+            console.log('https://api.vschool.io/matthewgrahek/todo/'+data[postNumber]._id);
 
+            axios.delete('https://api.vschool.io/matthewgrahek/todo/'+data[postNumber]._id).then(response => {
+                console.log(response.data);
+              }).catch(error => {
+                console.log(error)
+              });
+
+            e.target.parentNode.remove();
+        });
+    }
+
+    for (var editButton of editButtons) {
+        editButton.addEventListener('click', function (e) {
+            var postNumber = parseInt(e.target.parentNode.querySelector('.itemNumber').innerText);
+            if (e.target.innerText == "Edit") {
+                e.target.innerHTML = "Save";
+                e.target.parentNode.querySelector('.itemTitle').contentEditable = "true";
+                e.target.parentNode.querySelector('.itemDescription').contentEditable = "true";
+                data[postNumber].title = e.target.parentNode.querySelector('.itemTitle').innerText;
+                data[postNumber].description = e.target.parentNode.querySelector('.itemDescription').innerText;
+                axios.put('https://api.vschool.io/matthewgrahek/todo/'+data[postNumber]._id, data[postNumber]).then(response => {
+                    console.log(response.data);
+                  }).catch(error => {
+                    console.log(error)
+                  });
+            } else {
+                data[postNumber].title = e.target.parentNode.querySelector('.itemTitle').innerText;
+                data[postNumber].description = e.target.parentNode.querySelector('.itemDescription').innerText;
+                axios.put('https://api.vschool.io/matthewgrahek/todo/'+data[postNumber]._id, data[postNumber]).then(response => {
+                    console.log(response.data);
+                  }).catch(error => {
+                    console.log(error)
+                  });
+                e.target.innerHTML = "Edit";
+                e.target.parentNode.querySelector('.itemTitle').contentEditable = "false";
+                e.target.parentNode.querySelector('.itemDescription').contentEditable = "false";
+            }
+        });
+    }
+
+    for (var checkbox of checkboxes) {
+        checkbox.addEventListener('change', function (e) {
+            var postNumber = parseInt(e.target.parentNode.querySelector('.itemNumber').innerText);
+            if (this.checked) {
+                e.target.parentNode.style.opacity = .5;
+                data[postNumber].completed=true;
+                axios.put('https://api.vschool.io/matthewgrahek/todo/'+data[postNumber]._id, data[postNumber]).then(response => {
+                    console.log(response.data);
+                  }).catch(error => {
+                    console.log(error)
+                  });
+            } else {
+                e.target.parentNode.style.opacity = 1;
+                data[postNumber].completed=false;
+                axios.put('https://api.vschool.io/matthewgrahek/todo/'+data[postNumber]._id, data[postNumber]).then(response => {
+                    console.log(response.data);
+                  }).catch(error => {
+                    console.log(error)
+                  });
+            }
+
+        });
+
+    }
 }
 
-document.addEventListener("DOMContentLoaded", getData());
-function getData() {
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            var jsonData = xhr.responseText;
-            data = JSON.parse(jsonData);
-            console.log(data);
-            parseDataToWebsite(data);
-        }
-    };
-    xhr.open("GET", "https://api.vschool.io/matthewgrahek/todo", true);
-    xhr.send();
+function assignResponseToVariable(response){
+    data = response.data;
+    parseDataToWebsite(data);
+    addFunctionality();
+    console.log(data);
 }
 
+function createPost(title,descrip,url){
+    this.title=title;
+    this.description=descrip;
+    this.imgUrl = url;
+    this.completed=false;
+}
 
-function deleteChildNodes(target){
-
+function sendData(newTodo){
+    console.log(newTodo);
+    axios.post('https://api.vschool.io/matthewgrahek/todo', newTodo).then(response => {
+    console.log(response.data);
+    data =[];
+    deletePosts();
+    getData();
+    //data.push(response.data);
+    //createIndividualPost(response.data,data.length);
+    //addFunctionality();
+  }).catch(error => {
+    console.log(error);
+  });
+}
+function deletePosts(){
+    while (listContainer[0].hasChildNodes()) {  
+        listContainer[0].removeChild(listContainer[0].firstChild);
+      }
 }
 
 function parseDataToWebsite(items) {
@@ -50,14 +143,14 @@ function createIndividualPost(post, number) {
 
     var itemNumber = document.createElement("div");
     itemNumber.classList.add("itemNumber");
-    itemNumber.innerHTML = parseInt(number) + 1 + ". ";
+    itemNumber.innerHTML = parseInt(number) + ". ";
 
     var itemTitle = document.createElement("div");
     itemTitle.classList.add("itemTitle");
-    itemTitle.innerText = (post.title === undefined) ? "Untitled": " "+ post.title;
+    itemTitle.innerText = (post.title === undefined) ? "Untitled" : " " + post.title;
 
     var img = document.createElement("img");
-    (post.imgUrl === undefined) ? img.style.display ='none' : img.src = post.imgUrl;
+    (post.imgUrl === undefined) ? img.style.display = 'none' : img.src = post.imgUrl;
 
     var itemDescription = document.createElement("div");
     itemDescription.classList.add("itemDescription");
@@ -69,7 +162,9 @@ function createIndividualPost(post, number) {
     var checkBox = document.createElement("input");
     checkBox.setAttribute("type", "checkbox");
     checkBox.classList.add("itemCheckBox");
-    
+    checkBox.checked=post.completed;
+    listItem.style.opacity = (post.completed)? .5:1;
+
 
     var editButton = document.createElement("button");
     editButton.classList.add("editButton");
@@ -88,7 +183,7 @@ function createIndividualPost(post, number) {
     listItem.appendChild(editButton);
     listItem.appendChild(deleteButton);
     listContainer[0].appendChild(listItem);
-    }
+}
 
 // const xhr = new XMLHttpRequest();
 // xhr.onreadystatechange = function () {
